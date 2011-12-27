3651f7a9d2c24e3d5e1e8b559962ac2e02d1a4f7
Ben Hocking <hocking@datamininglab.com>
2011-12-26 19:50:43 -0500

    Made Synapse.?pp mostly cpplint.py compliant

--- neural/Synapse.cpp
+++ neural/Synapse.cpp
@@ -1,9 +1,21 @@
 /***************************************************************************
- *            Synapse.cpp
+ * Synapse.cpp
  *
- *  Thu Jun  9 12:05:45 2005
- *  Copyright  2005  User
- *  Email
+ *  Copyright 2005, 2009, 2011 Informed Simplifications, LLC
+ *  This file is part of NeuroJet.
+ *
+ *  NeuroJet is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  NeuroJet is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with NeuroJet.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
  ****************************************************************************/
 
 #if !defined(SYNAPSE_HPP)
